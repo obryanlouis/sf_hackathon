17abffcc232eddd1a74a31fffa3f0a00c301b892
Reza Jelveh <toyb0mb@gmx.net>
2015-09-19 23:18:52 -0700

    yeah i'm actually putting an api key in here

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+var apiKey = "qs9s5dj9w6z4bj2gws282k8r"
+
 var Hapi = require('hapi');
+var walmart = require('walmart')(apiKey);
 
 // Create a server with a host and port
 var server = new Hapi.Server();
@@ -13,6 +16,19 @@ server.route({
     path:'/hello', 
     handler: function (request, reply) {
         reply('hello world');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/walmart',
+    handler: function (request, reply) {
+      walmart.search(request.query.search, {'facet': 'on', 'facet.range': 'price:[150 TO 1200]'}).then(function(item) {
+          walmart.recommendations(item.items[0].itemId).then(function(recommendation) {
+            reply({items: item.items, recommendations: recommendation});
+          });
+      });
+
     }
 });
 
